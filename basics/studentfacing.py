018cdea18579b39c22ae9e4b48fa33989c8376ab
Chloe Simanek <chloesimanek@gmail.com>
2025-10-03 20:13:42 -0500

    student facing part for basics assignment

---
+++ basics/studentfacing.py
@@ -0,0 +1,40 @@
+import workinfo as wi 
+'''
+workplanner.py
+
+Ava Amthauer and Chloe Simanek
+
+Description:
+In this file you will create a functioning payment calculator for a 
+gig-delivery-driver that takes variables like which days they are working and
+where they are taking jobs from as factors that would impact their daily
+pay.  This calculator will also take into consideration the amount that the
+driver would have to spend on gas and takes that out of their earnings
+so that they can see the net value of what they would make based on the 
+values that they select.
+
+'''
+
+'''
+Part 1: Getting Started with Variables
+
+Define variables for base pay, weather, location and more.
+'''
+
+# Company set variables go here
+
+# Driver set variables go here 
+
+
+'''
+Part 2: Calculations
+'''
+# Complete the calculations for the amount being deducted because of fuel use
+# Complete the calculations for the amount that the driver would be getting paid
+# with the given multipliers for the amount they are working
+
+'''
+Part 3: Tying it Together, User Input
+'''
+# For any option that can be changed by the user, make it so that it
+# can take user input from the terminal
