6d9b68c223c224b4de22e661bcaa5e80ac66086a
Chloe Simanek <chloesimanek@gmail.com>
2025-10-07 14:53:52 -0500

    cleaned data

--- healthcareData/healthcare.py
+++ healthcareData/healthcare.py
@@ -11,7 +11,7 @@ Description:
 Part 1: Know Your Data
 '''
 
-filename = "healthcare_dataset.csv"
+filename = "healthcare_dataset_clean.csv"
 
 # Lists to store each column
 names = []
@@ -23,9 +23,6 @@ medical_conditions = []
 dates_of_admission = []
 doctors = []
 hospitals = []
-insurance_providers = []
-billing_amounts = []
-room_numbers = []
 admission_types = []
 discharge_dates = []
 medications = []
@@ -47,17 +44,15 @@ with open(filename) as file:
         dates_of_admission.append(fields[5])
         doctors.append(fields[6])
         hospitals.append(fields[7])
-        insurance_providers.append(fields[8])
-        billing_amounts.append(fields[9])
-        room_numbers.append(fields[10])
-        admission_types.append(fields[11])
-        discharge_dates.append(fields[12])
-        medications.append(fields[13])
-        test_results.append(fields[14])
+        admission_types.append(fields[8])
+        discharge_dates.append(fields[9])
+        medications.append(fields[10])
+        test_results.append(fields[11])
 
 # Print the first row 
-print(names[1], ages[1], genders[1], blood_types[1], medical_conditions[1], dates_of_admission[1], doctors[1], hospitals[1], insurance_providers[1], billing_amounts[1], room_numbers[1], admission_types[1], discharge_dates[1], medications[1], test_results[1])  # add other fields here if needed
-
+print(names[0], ages[0], genders[0], blood_types[0], medical_conditions[0],
+      dates_of_admission[0], doctors[0], hospitals[0], admission_types[0],
+      discharge_dates[0], medications[0], test_results[0])
 # Print all medical conditions 
 # for condition in medical_conditions:
     # print(condition)
@@ -111,7 +106,19 @@ male_percent = (male / total) * 100
 
 # Hospital / Doctor representation
 
+total = len(hospitals)
+hospital_counts = {}
 
+for h in hospitals:
+    if h in hospital_counts:
+        hospital_counts[h] += 1
+    else:
+        hospital_counts[h] = 1
+
+# Print percentages
+for h, count in hospital_counts.items():
+    percent = (count / total) * 100
+    print(f"{h}: {percent:.2f}%")
 
 # NOTE: Discussion 
 
