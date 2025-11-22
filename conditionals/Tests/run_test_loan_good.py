bcd6f41e886e6bab2e7bf1662466772c078f74d8
aamthauer <amthauera@carleton.edu>
2025-11-22 13:02:20 -0500

    Add test script for loanApproval_good.py
    This script runs tests on loanApproval_good.py using Pytest and Pylint, and saves the results to a text file.

---
+++ conditionals/Tests/run_test_loan_good.py
@@ -0,0 +1,24 @@
+"""
+Description: Script to test loanApproval_good.py and and the test file
+using Pytest and Pylint. Results written to a text file.
+"""
+
+import subprocess
+
+pytest_command = ["python", "-m", "pytest", "-v", "test_loan_good.py"]
+pylint_command = ["python", "-m", "pylint", "--rcfile=.pylintrc", "../ProductionCode/loanApproval_good.py"]
+
+run_pytest = subprocess.run(pytest_command, capture_output=True, text=True)
+run_pylint = subprocess.run(pylint_command, capture_output=True, text=True)
+
+file = "test_results_loan_good.txt"
+
+with open(file, "w", encoding="utf-8") as f:
+    f.write("Test Functions (Pytest)\n\n")
+    f.write(run_pytest.stdout)
+    f.write("\n\nTest Style (Pylint)\n\n")
+    f.write(run_pylint.stdout)
+    f.write("\n")
+
+print("Testing complete.")
+print(f"Results written to: {file}")
